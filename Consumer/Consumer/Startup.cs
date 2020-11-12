c66486bcc8ed6d4ced083f12988b1525d166c455
Artem Sergeev <aesergnsk@gmail.com>
2020-11-12 16:33:43 +0700

    Consumer added

--- Consumer/Consumer/Startup.cs
+++ Consumer/Consumer/Startup.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using MassTransit;
+using MassTransit.AspNetCoreIntegration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace Consumer
 {
@@ -26,6 +22,36 @@ namespace Consumer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMassTransit(isp =>
+                {
+                    var hostConfig = new MassTransitConfiguration();
+                    Configuration.GetSection("MassTransit").Bind(hostConfig);
+
+                    return Bus.Factory.CreateUsingRabbitMq(cfg =>
+                    {
+                        var host = cfg.Host(
+                            new Uri(hostConfig.RabbitMqAddress), 
+                            h =>
+                            {
+                                h.Username(hostConfig.UserName);
+                                h.Password(hostConfig.Password);
+                            });
+
+                        cfg.Durable = hostConfig.Durable;
+                        cfg.PurgeOnStartup = hostConfig.PurgeOnStartup;
+
+                        cfg.ReceiveEndpoint(host,
+                            "test-queue-name", ep =>
+                            {
+                                ep.PrefetchCount = 1;
+                                ep.ConfigureConsumer<PaymentReceivedConsumer>(isp);
+                            });
+                    });
+                },
+                ispc =>
+                {
+                    ispc.AddConsumers(typeof(PaymentReceivedConsumer).Assembly);
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
